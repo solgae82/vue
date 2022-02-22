b8ab671e9444ea7d129e658f42e849b17191abb2
solgae82 <kehdgur@gmail.com>
2022-02-22 21:31:17 +0900

    pc

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+<<<<<<< HEAD
+Vue.component('child-component',{
+  props: ['propsdata'],
+  template:'<p>{{propsdata}}</p>'
+});
+
+var app = new Vue({
+
+  el : '#app',
+  data: {
+    message:'Hello Vue! passed from Parent Component',
+    anotherMessage: '다른 메시지'
+  }
+});
+=======
 var app = null;
 var app2 = null;
+>>>>>>> 2df9fc6903236e9f60424619b841d081f2a8af54
